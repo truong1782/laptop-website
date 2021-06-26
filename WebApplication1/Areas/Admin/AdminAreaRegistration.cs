d8f6c6d976787c4b730a40f2a0d5ca2dc0ab384c
truong <quangtruong.tdr012@gmail.com>
2021-06-27 01:42:47 +0700

    update RoutConfig

--- WebApplication1/Areas/Admin/AdminAreaRegistration.cs
+++ WebApplication1/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace WebApplication1.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "WebApplication1.Areas.Admin.Controllers" }
             );
         }
     }
