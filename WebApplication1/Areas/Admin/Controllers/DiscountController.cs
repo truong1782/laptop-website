91f0343128bd3fa93eaba4c34941941aa3ee931f
truong <quangtruong.tdr012@gmail.com>
2021-06-27 00:29:40 +0700

    test pull request

---
+++ WebApplication1/Areas/Admin/Controllers/DiscountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Areas.Admin.Controllers
+{
+    public class DiscountController : Controller
+    {
+        // GET: Admin/Discount
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
