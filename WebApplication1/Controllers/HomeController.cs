1de6fcee14c3d40459cc343cd417d2ccc2fa3104
truong <quangtruong.tdr012@gmail.com>
2021-06-28 22:52:36 +0700

    Tao trang San pham va trang Chi tiet san pham

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Home()
+        public ActionResult Index()
         {
             return View();
         }
--- WebApplication1/Controllers/ProductDetailController.cs
+++ WebApplication1/Controllers/ProductDetailController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using WebApplication1.Models.Data;
 namespace WebApplication1.Controllers
 {
     public class ProductDetailController : Controller
     {
         // GET: ProductDetail
-        public ActionResult Index()
+        DBContext db = new DBContext();
+        public ActionResult Index(int id)
         {
-            return View();
+            ViewBag.Products = db.Product.Take(4).ToList();
+            return View(db.Product.Find(id));
         }
     }
 }
