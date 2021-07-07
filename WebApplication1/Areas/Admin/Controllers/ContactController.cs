66b4b9e8a9d0f834771ed11fd0267da23696649b
truong <quangtruong.tdr012@gmail.com>
2021-07-07 20:44:30 +0700

    hoan thanh trang quan ly khuyen mai

--- WebApplication1/Areas/Admin/Controllers/ContactController.cs
+++ WebApplication1/Areas/Admin/Controllers/ContactController.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Models.Data;
+using WebApplication1.Services;
 namespace WebApplication1.Areas.Admin.Controllers
 {
     public class ContactController : Controller
     {
         // GET: Admin/Contact
         DBContext db = new DBContext();
+        EmailService emailService = new EmailService();
         public ActionResult listContact()
         {
             var listContact = db.Contacts.ToList();
@@ -30,9 +34,26 @@ namespace WebApplication1.Areas.Admin.Controllers
             return RedirectToAction("listContact", "Contact");
         }
 
-        public ActionResult feedback()
+        [HttpGet]
+        public ActionResult feedback(int id)
         {
-            return RedirectToAction("listContact");
+            var contact = db.Contacts.Find(id);
+            return View(contact);
+        }
+
+        [HttpPost]
+        public ActionResult feedback(FormCollection frm, int id)
+        {
+            string Address = frm["address"];
+            string Title = frm["title"];
+            string Message = frm["message"];
+            emailService.sendEmail(Address, Title, Message);
+
+            var contact = db.Contacts.Find(id);
+            contact.status = true;
+            db.SaveChanges();
+
+            return RedirectToAction("listContact", "Contact");
         }
     }
 }
---
+++ WebApplication1/Services/EmailService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace WebApplication1.Services
+{
+    public class EmailService
+    {
+        public void sendEmail(string emailAddress, string title, string message)
+        {
+            string myEmail = "thtlaptopstore@gmail.com";
+            string myPassword = "truonghieutruong@123";
+
+            var loginInfo = new NetworkCredential(myEmail, myPassword);
+            var msg = new MailMessage();
+            var smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+            msg.From = new MailAddress(myEmail);
+            msg.To.Add(new MailAddress(emailAddress));
+            msg.Subject = title;
+            msg.Body = message;
+            msg.IsBodyHtml = true;
+
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = loginInfo;
+            smtpClient.Send(msg);
+        }
+    }
+}
